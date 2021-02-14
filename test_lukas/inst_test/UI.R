2e80cc6a95565de8a8cd3c5cc8de4870c883bec1
Lukas Brunner <lukas.brunner@student.uni-tuebingen.de>
2021-02-14 19:10:44 +0100

    Update UI.R

--- test_lukas/inst_test/UI.R
+++ test_lukas/inst_test/UI.R
@@ -1,14 +1,5 @@
 fluidPage(
-  selectInput("dataset", label = "Dataset", choices = ls("package:datasets")),
-
-  verbatimTextOutput("summary"),
-  #verbatimTextOutput("sum_model"),
-  textOutput("path"),
-  verbatimTextOutput("test_table"),
-  DT::DTOutput("test_table_dt"),
-  tableOutput("test_table_df"),
-  tableOutput("table"),
-  plotOutput("plot1")
+  selectizeInput('foo', choices = NULL, ...)
 
 
 )
