695f8b8bdc1adb86ae4f7d324e291747c660f4a9
Lukas Brunner <lukas.brunner@student.uni-tuebingen.de>
2021-02-14 14:13:00 +0100

    add test files

---
+++ R_test/load_data.R
@@ -0,0 +1,35 @@
+
+#' Test Functions
+#' @export
+#' @rdname test_data
+test_data <- function(){
+  filename <- system.file("data", "beerSalesSubset.csv", package = "SentimentApp")
+  #filename <- "data/beerSalesSubset.csv"
+  #filename <- "https://unitc-my.sharepoint.com/:x:/g/personal/zxmvp94_s-cloud_uni-tuebingen_de/ESTGmTK06PJEgx5bZ99GploBA6csaAbkLRTdmpMoXDnP9A?download=1"
+  test_data1 <- read.csv(filename)
+
+
+
+  #test_data1 <- get("iris", "package:datasets")
+
+
+}
+
+
+
+
+
+getData <- function(test_data1){
+  head(test_data1)
+}
+
+plotdata <- function(test_data1){
+  ggplot(test_data1, aes(price_ounce, move_ounce)) +
+    geom_point()
+}
+
+
+
+
+
+
