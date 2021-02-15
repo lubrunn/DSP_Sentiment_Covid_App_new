c3f4a883c219cbef6cc2c8bd94acf36efaebad1c
Kai Moritz <kai-moritz.brehm@student.uni-tuebingen.de>
2021-02-15 18:10:48 +0100

    stock

--- test_kai/inst_test/UI.R
+++ test_kai/inst_test/UI.R
@@ -1,14 +1,7 @@
 fluidPage(
-  selectInput("dataset", label = "Dataset", choices = ls("package:datasets")),
-
-  verbatimTextOutput("summary"),
-  #verbatimTextOutput("sum_model"),
-  textOutput("path"),
-  verbatimTextOutput("test_table"),
-  DT::DTOutput("test_table_dt"),
-  tableOutput("test_table_df"),
-  tableOutput("table"),
-  plotOutput("plot1")
+  selectInput("stock", label = "Chose COmpany", choices = ls("package:datasets")),
+  plotOutput("plot")
 
 
 )
+
