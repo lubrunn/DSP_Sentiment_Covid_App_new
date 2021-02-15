c1f500ac066670b9f83194c836d5e70eb69dd848
Simon Hassler <simon.hassler@student.uni-tuebingen.de>
2021-02-15 09:52:22 +0100

    aggregation function for sentiment

--- test_simon/R_test/aggregate_sentiment.R
+++ test_simon/R_test/aggregate_sentiment.R
@@ -6,10 +6,20 @@
 #' @rdname dataPreps
 aggregate_sentiment <- function(test_data) {
 
-  iris %>%
-    group_by(Species) %>%
-    summarise_at(vars(Sepal.Length),
-                 list(name = mean))
+  test_data <- test_data %>%
+    group_by(date,language,folder) %>%
+   summarise(mean_weight_retweet = weighted.mean(sentiment,
+                                               retweets_count/
+                                                  sum(retweets_count)),
+             mean_weight_likes = weighted.mean(sentiment,
+                                               likes_count/
+                                                 sum(likes_count)),
+             mean_weight_length = weighted.mean(sentiment,
+                                                tweet_length/
+                                                  sum(tweet_length)),
+             mean_no_weight = mean(sentiment))
 
+
+  test_data <- test_data[order(test_data$folder,test_data$date),]
 
 }
