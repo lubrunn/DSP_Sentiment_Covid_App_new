6a55af59c8c2cb7dcb242606443cc5ffc0d492ce
Simon Hassler <simon.hassler@student.uni-tuebingen.de>
2021-02-15 16:56:18 +0100

    add new filter options + add new plottype + change shiny structure

--- test_simon/SimonApp/R_test/test_data.R
+++ test_simon/SimonApp/R_test/test_data.R
@@ -20,14 +20,3 @@ test_data <- function(){
 
 
 
-# getData <- function(test_data1){
-#   head(test_data1)
-# }
-#
-# plotdata <- function(test_data1){
-#   ggplot(test_data1, aes(price_ounce, move_ounce)) +
-#     geom_point()
-# }
-#
-
-
--- test_simon/SimonApp/inst_test/server.R
+++ test_simon/SimonApp/inst_test/server.R
@@ -1,23 +1,45 @@
 
 
 function(input, output, session) {
-  # getExampleData <- reactive({
-  #   test_data()
-  # })
+
+    filtered_df <- reactive({
+     res <- test_data()  %>% filter(folder == input$tweetType)
+     res <- res %>% filter( (date > input$timeWindow[1]) &
+                             (date < input$timeWindow[2]))
+    })
+
+    filtered_df_min_retweet <- reactive({
+      res <- filtered_df()
+      res <- res %>% filter(retweets_count > input$minRetweet)
+    })
+
+    outVar <- reactive({
+      test_data <- test_data()
+      sort(unique(as.character(test_data$folder)))
+    })
+
+    observe({
+      updateSelectInput(session, "tweetType", choices = outVar())
+    })
 
 
-  outVar <- reactive({
-    test_data <- test_data()
-    sort(unique(as.character(test_data$folder)))
-  })
+   max_retweet <- reactive(({
+    test_data <- filtered_df()
+    max_val_vec <- test_data %>% group_by(date) %>%  summarise(maxi = max(retweets_count))
+    min(max_val_vec$maxi)
 
-  observe({
-    updateSelectInput(session, "tweetType", choices = outVar()) #data()$customerID #c(output$outVar)
-  })
+    }))
+
+   observe({
+    updateSliderInput(session, "minRetweet", max = max_retweet())
+    })
+
+    output$plot1 <- renderPlot({
+      TS_plot(filtered_df_min_retweet(), aggregation = input$aggregation)})
+
+    output$plot2 <- renderPlot({
+      density_plot(filtered_df_min_retweet(), aggregation = input$aggregation)})
 
 
-  output$plot1 <- renderPlot({
-    TS_plot(test_data(),tweetType = input$tweetType, aggregation = input$aggregation)
-  })
 
 }
