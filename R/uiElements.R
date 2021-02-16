7b8f999cb7057001acbac8dc50b8aa5d1b6658dd
Kai Moritz <kai-moritz.brehm@student.uni-tuebingen.de>
2021-02-16 14:14:38 +0100

    uiElements revise

--- R/uiElements.R
+++ R/uiElements.R
@@ -6,6 +6,8 @@
 
 #####################################################   Stocks
 # selectize input for companies
+#' @export
+#' @rdname uiElements
 selectize_Stocks <- function() {
   selectizeInput("Stock","Chose Company",
                  c(COMPONENTS_DE()[["Company.Name"]]),
@@ -13,6 +15,8 @@ selectize_Stocks <- function() {
                  selected = "")
 }
 # sliderinput for dates
+#' @export
+#' @rdname uiElements
 sliderinput_dates <- function(){
   sliderInput("dates",label="Timeseries",
               value = c(median(as.Date(ADS()[["Date"]], "%b %d, %Y")),max(as.Date(ADS()[["Date"]], "%b %d, %Y"))),
@@ -21,18 +25,29 @@ sliderinput_dates <- function(){
               step = 1,timeFormat = "%F")
 }
 #plotoutput for german companies
+#' @export
+#' @rdname uiElements
 plot_stocks_DE <- function() {
   plotOutput("plot_DE",hover = hoverOpts("plot_hover_DE", delay = 10, delayType = "debounce"))
 }
 #hoverbox in german plot
+#' @export
+#' @rdname uiElements
 hover_info_DE <- function() {
   uiOutput("hover_info_DE")
 }
 #plotoutput for us commpanies
+#' @export
+#' @rdname uiElements
 plot_stocks_US <- function() {
   plotOutput("plot_US",hover = hoverOpts("plot_hover_US", delay = 10, delayType = "debounce"))
 }
 #hoverbox in us plot
+#' @export
+#' @rdname uiElements
 hover_info_US <- function() {
   uiOutput("hover_info_US")
 }
+
+
+
