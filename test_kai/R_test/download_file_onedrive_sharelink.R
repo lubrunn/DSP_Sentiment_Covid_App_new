1c6a855cc11a134fb58c4c9be480981756200686
Kai Moritz <kai-moritz.brehm@student.uni-tuebingen.de>
2021-02-16 11:07:22 +0100

    sort testfolderkai

--- test_kai/R_test/download_file_onedrive_sharelink.R
+++
@@ -1,6 +0,0 @@
-
-filename_orig <- "https://unitc-my.sharepoint.com/:x:/g/personal/zxmvp94_s-cloud_uni-tuebingen_de/ESTGmTK06PJEgx5bZ99GploBA6csaAbkLRTdmpMoXDnP9A?e=okeMyu"
-filename_edit <- "https://unitc-my.sharepoint.com/:x:/g/personal/zxmvp94_s-cloud_uni-tuebingen_de/ESTGmTK06PJEgx5bZ99GploBA6csaAbkLRTdmpMoXDnP9A?download=1"
-
-
-test2 <- read.csv(filename_edit)
