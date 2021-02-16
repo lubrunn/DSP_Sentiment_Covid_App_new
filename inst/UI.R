78305ff3ca2870cdc9d4944c76234d4ca51613d8
Kai Moritz <kai-moritz.brehm@student.uni-tuebingen.de>
2021-02-16 16:42:15 +0100

    UI revised

--- inst/UI.R
+++ inst/UI.R
@@ -1,27 +1,30 @@
 Sys.setlocale("LC_TIME", "English")
+
 fluidPage(
+  theme = shinythemes::shinytheme("slate"),
+  #shinythemes::themeSelector(),
+  titlePanel("Sentiment_Covid_App"),
   tabsetPanel(
     tabPanel("Twitter"),
     tabPanel("Sentiment"),
     tabPanel("Stocks",
-             tabsetPanel(
-               tabPanel("Germany",
-                        sidebarLayout(
-                          sidebarPanel(
-                            selectize_Stocks(COMPONENTS_DE()),
-                            actionButton("reset", "clear selected"),
-                            sliderinput_dates()
-                          ),
-                          mainPanel(
-                            plot_stocks_DE(),
-                            hover_info_DE(),
-                            plot_stocks_US(),
-                            hover_info_US()
-                          )#close mainpanel
-                        )#close sidebarlayout
-               ),#close tabPanel Germany
-               tabPanel("USA")
-             )#close tabsetPanel
-    )#close tabPanel stock
+             sidebarPanel(
+               selectize_Stocks(COMPONENTS_DE()),
+               actionButton("reset", "clear selected"),
+               sliderinput_dates()
+             ),
+             mainPanel(
+               tabsetPanel(
+                 tabPanel("Germany",
+                          plot_stocks_DE(),
+                          hover_info_DE(),
+                          plot_stocks_US(),
+                          hover_info_US()
+                          ),#close tabPanel Germany
+                 tabPanel("USA")
+                 )#close tabsetPanel
+               )#close mainpanel
+             )#close tabPanel stock
   )#close tabsetPanel
 )#close fluidpage
+
