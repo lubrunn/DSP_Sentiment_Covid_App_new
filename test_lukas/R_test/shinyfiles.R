75c8ebec6ecb8ddee8aea2d49ee8cba14a923b32
Lukas Brunner <lukas_brunner95@yahoo.de>
2021-02-25 15:14:10 +0100

    Create shinyfiles.R

---
+++ test_lukas/R_test/shinyfiles.R
@@ -0,0 +1,21 @@
+install.packages('shinyFiles')
+
+library(shinyFiles)
+
+shinyFilesExample()
+
+
+
+
+
+devtools::install_github('wleepang/shiny-directory-input')
+
+library(shinyDirectoryInput)
+shinyDirectoryInput::runDirinputExample()
+
+library(shiny)
+
+
+# shiny helper
+shinyhelper::shinyhelper_demo()
+
