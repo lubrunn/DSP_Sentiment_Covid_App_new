5d974cb100aeadaf0dea4fa6eaefad7cde803374
Simon Hassler <simon.hassler@student.uni-tuebingen.de>
2021-02-28 01:20:29 +0100

    add newest version + quantile regression

--- test_simon/SimonApp/R_test/use_package.R
+++ test_simon/SimonApp/R_test/use_package.R
@@ -2,4 +2,5 @@ usethis::use_package("ggplot2")
 usethis::use_package("feather")
 usethis::use_package("arrow")
 usethis::use_package("dplyr")
+usethis::use_package("ggbeeswarm")
 
---
+++ test_simon/aktuelle_version/R_test/NAMESPACE.R
@@ -0,0 +1,10 @@
+#' @import ggplot2
+#' @import shiny
+#' @import dplyr
+#' @import vars
+#' @import tseries
+#' @import tidyr
+#' @import imputeTS
+#' @import glue
+#' @import quantreg
+NULL
