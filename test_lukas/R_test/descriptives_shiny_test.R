c1e10aaf0f4af3fd7b960f269e6cf651499dee30
Lukas Brunner <lukas_brunner95@yahoo.de>
2021-02-28 23:45:16 +0100

    Create descriptives_shiny_test.R
    for testing shiny process

---
+++ test_lukas/R_test/descriptives_shiny_test.R
@@ -0,0 +1,70 @@
+querry_str <- "SELECT likes_count, sum(N) as  n  FROM histo_rt_en  WHERE date >= '2018-11-30' and date <= '2021-02-13'
+and retweets_count_filter = 0 and likes_count_filter = 0 and
+tweet_length_filter = 0
+group by likes_count
+"
+
+
+
+
+old_wd <- getwd()
+setwd("C:/Users/lukas/OneDrive - UT Cloud/Data/SQLiteStudio/databases")
+con <- DBI::dbConnect(RSQLite::SQLite(), "test.db")
+setwd(old_wd)
+time1 <- Sys.time()
+df_need <- DBI::dbGetQuery(con, querry_str)
+print(Sys.time() -  time1)
+
+
+#disconnect
+DBI::dbDisconnect(con)
+
+
+
+df_need %>%
+  # filter(likes_count < 10000
+  #         # likes_count > 0
+  #        ) %>%
+
+
+
+
+
+  mutate(log_metric = log(likes_count + 0.0001),
+    bins = cut_interval(log_metric, n = 100)) %>%
+ggplot(aes(bins, n)) +
+  geom_col() +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+
+
+
+
+########## time series
+querry_str <- "SELECT date, N as value FROM sum_stats_en  WHERE date >= '2018-11-30' and date <= '2021-02-19'
+and retweets_count = 0 and likes_count = 0 and
+tweet_length = 0"
+
+
+old_wd <- getwd()
+setwd("C:/Users/lukas/OneDrive - UT Cloud/Data/SQLiteStudio/databases")
+con <- DBI::dbConnect(RSQLite::SQLite(), "test.db")
+setwd(old_wd)
+time1 <- Sys.time()
+df_need <- DBI::dbGetQuery(con, querry_str)
+print(Sys.time() -  time1)
+
+
+#disconnect
+DBI::dbDisconnect(con)
+
+
+# convert date to date
+df_need$date <- as.Date(df_need$date)
+
+df_need %>%
+  ggplot() +
+  geom_line(aes(date, value))
+
+
+
+a
