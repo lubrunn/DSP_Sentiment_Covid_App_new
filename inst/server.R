9d476e6890bef7737e1c87ad2239c1e8685518c1
Lukas Brunner <lukas_brunner95@yahoo.de>
2021-03-01 15:29:51 +0100

    Update server.R
    connected dir_path_setter with database connecion,
    include all of twitter_descriptive

--- inst/server.R
+++ inst/server.R
@@ -466,25 +466,232 @@ server <- function(input, output, session) {
 
       path <- shinyFiles::parseDirPath(volumes, input$directory)
       setwd(path)
+      con <- DBI::dbConnect(RSQLite::SQLite(), "SQLiteStudio/databases/test.db")
+
       file_needed <- "SQLiteStudio"
       if(dir.exists(file_needed)) {
         #setwd(file_path)
-        glue("Current path {getwd()}")
+        output_str <- glue("Current path {getwd()}")
       } else {
         #setwd(file_path)
-        "Current path selection does not seem correct. \n
+        output_str <- "Current path selection does not seem correct. \n
                 Are you sure it is set correctly?"
       }
 
-
+      path_outputs <- list(a = con, b = output_str)
+      path_outputs
 
 
     }
   })
   output$directorypath <- renderText({
-    path_setter()
+    path_outputs <- path_setter()
+    path_outputs[[2]][1]
   })
 
+
+
+
+
+  ###############################################################################
+  ############################### twitter descriptive ###########################
+  ###############################################################################
+  ### for histogram less choices
+  observeEvent(input$plot_type,{
+
+    if (input$plot_type == "histo"){
+      if (input$value %in% c("sentiment_rt", "sentiment_likes", "sentiment_length")){
+        selected_value <-"sentiment"
+      } else {
+        selected_value <- input$value
+      }
+
+      updateSelectInput(session = session, "value",
+                        choices = c("Sentiment" = "sentiment",
+                                    "Retweets" = "rt",
+                                    "Likes"="likes",
+                                    "Tweet Length" = "tweet_length"
+                        ), selected = selected_value)
+    } else {
+
+      updateSelectInput(session = session, "value",
+                        choices = c(
+                          "Sentiment" = "sentiment",
+                          "Retweets Weighted Sentiment" = "sentiment_rt",
+                          "Likes Weighted Sentiment" = "sentiment_likes",
+                          "Length Weighted Sentiment" = "sentiment_tweet_length",
+                          "Retweets" = "rt",
+                          "Likes"="likes",
+                          "Tweet Length" = "tweet_length",
+                          "Number of Tweets" = "N"
+                        ), selected = input$value)
+    } #if clause
+  }) #observeevent
+
+  # avoid that date range upper value can be lower than lower value
+  # Update the dateRangeInput if start date changes
+  observeEvent(input$dates[1], {
+    end_date = input$dates[2]
+    # If end date is earlier than start date, update the end date to be the same as the new start date
+    if (input$dates[2] < input$dates[1]) {
+      end_date = input$dates[1]
+    }
+    updateDateRangeInput(session,"dates", start=input$dates[1], end=end_date, min=input$dates[1] )
+  })
+
+
+  ######## disconnect from database after exit
+  cancel.onSessionEnded <- session$onSessionEnded(function() {
+    DBI::dbDisconnect(con)
+  })
+
+
+
+
+  querry <- reactive({
+
+    if (input$long == T){
+      long <- 81
+    } else{
+      long <- 0
+    }
+    #browser()
+    if (input$plot_type == "sum_stats"){
+
+      if (input$value == "N"){
+        metric <- "N"
+      } else if (input$value == "tweet_length"){
+        metric <- glue("{input$metric}_length")
+      } else{
+        metric <- glue("{input$metric}_{input$value}")
+      }
+
+
+
+      table_name <- glue("{input$plot_type}_{tolower(input$lang)}_all")
+      # browser()
+      glue("SELECT created_at, {metric} as value FROM {table_name}  WHERE created_at >= '{input$dates[1]}'
+      and created_at <= '{input$dates[2]}'
+         and retweets_count = {input$rt} and likes_count = {input$likes} and
+         tweet_length = {long}" )
+
+
+    } else if (input$plot_type == "histo"){
+      #browser()
+      if (input$value == "length"){
+        tb_metric <- "len"
+        col_value <- input$value
+
+      } else if(input$value == "rt") {
+
+        tb_metric <- input$value
+        col_val <- "retweets_count"
+
+      } else if(input$value == "likes") {
+        tb_metric <- input$value
+        col_val <- "likes_count"
+      } else if(input$value == "sentiment"){
+        tb_metric <- input$value
+        col_val <- "sentiment_rd"
+      } else{
+        Sys.sleep(0.2)
+      }
+
+
+
+
+
+      table_name <- glue("{input$plot_type}_{tb_metric}_{tolower(input$lang)}")
+
+      if (table_name %in% c("histo_rt_en", "histo_likes_en", "histo_len_en")){
+        date_col <- "date"
+      } else{
+        date_col <- "created_at"
+      }
+
+      glue("SELECT {col_val}, sum(N) as  n  FROM {table_name}  WHERE {date_col} >=  '{input$dates[1]}'
+      and {date_col} <= '{input$dates[2]}'
+      and retweets_count_filter = {input$rt} and likes_count_filter = {input$likes} and
+      tweet_length_filter = {long}
+      group by {col_val}")
+
+    }
+    #browser()
+
+
+  })
+
+  data <- reactive({
+
+
+    con <- path_setter()
+    con <- con[[1]]
+    df_need <- DBI::dbGetQuery(con, querry())
+    df_need
+  })
+
+
+
+
+  output$sum_stats_plot <- renderPlot({
+
+    df <- data()
+
+
+    if(input$plot_type == "sum_stats"){
+
+      df$created_at <- as.Date(df$created_at)
+      df %>%
+        ggplot(aes(x = created_at,
+                   y = value)) +
+        geom_line()
+    }
+
+  })
+
+
+  observeEvent(input$value, {
+    #browser()
+    if (input$value == "sentiment") {
+      shinyWidgets::updateSwitchInput(session = session,
+                                      "log_scale",
+                                      disabled = T)
+    } else {
+      shinyWidgets::updateSwitchInput(session = session,
+                                      "log_scale",
+                                      disabled = F)
+    }
+  })
+
+
+
+  output$histo_plot <- renderPlot({
+    df <- data()
+
+    #freezeReactiveValue(input, "plot_type")
+
+    # if sentiment then disable log button because has negative values
+
+
+
+    if (input$plot_type == "histo"){
+
+      df %>%
+        # {if (input$log_scale == T) {} else {
+        #          mutate(bins = cut_interval(.[[1]], n = input$bins))
+        #        }
+        # }
+
+        mutate(metric = case_when(input$log_scale == T ~ log(as.numeric(.[[1]])+ 0.0001),
+                                  input$log_scale == F ~ as.numeric(.[[1]])),
+               bins = cut_interval(metric, n = input$bins))%>%
+
+        ggplot(aes(bins, n)) +
+        geom_col() +
+        theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+
+    }
+  })
 
 
 }
