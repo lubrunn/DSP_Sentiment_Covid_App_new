691225697e5a64978bc9e6fd87ddb041987b8622
Lukas Brunner <lukas_brunner95@yahoo.de>
2021-03-05 01:37:24 +0100

    Update time_series_plt.R
    accounted for all metric contiang tweet_length compared to metrics where its conly called length

--- R/time_series_plt.R
+++ R/time_series_plt.R
@@ -26,7 +26,8 @@ df <-     df %>%
         )
     )  %>%
     separate(col = metric, into = c("type", "metric"), sep = "_", remove = F, extra = "merge") %>%
-  mutate(metric = stringr::str_replace(metric, "length", "tweet_length")) %>%
+  { if (!grepl("tweet_length", selected_metrics)) mutate(metric = stringr::str_replace(.date[[metric]], "length",
+                                                                                       "tweet_length")) else .} %>%
 
   bind_rows(
     df %>% select(created_at, N) %>% pivot_longer(N,
