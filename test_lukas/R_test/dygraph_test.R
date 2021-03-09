576dfba231637a13c5146fd92c69811896c13737
Lukas Brunner <lukas_brunner95@yahoo.de>
2021-03-09 16:00:38 +0100

    Update dygraph_test.R

--- test_lukas/R_test/dygraph_test.R
+++ test_lukas/R_test/dygraph_test.R
@@ -90,3 +90,33 @@ ribbonData[increasing] <- 1
 
 dygraph(dyData) %>%
   dyRibbon(data = ribbonData, top = 0.1, bottom = 0.02)
+
+
+
+
+
+
+
+##### testing violin plots
+
+library(data.table)
+library(tidyverse)
+library(plotly)
+df <- fread("C:/Users/lukas/OneDrive - UT Cloud/Data/Twitter/plot_data/En_NoFilter/sum_stats_En_NoFilter_rt_200_li_200_lo_long_only.csv")
+
+
+
+df_test <- rbind(df1, df2)
+p <- df %>%
+  ggplot() +
+  geom_violin(aes(1,mean_rt))
+
+plotly::ggplotly(p)
+
+
+
+df1 <- df$mean_rt %>% data.frame()
+
+
+df1 %>% ggplot() +
+  geom_violin(aes_string(1, names(df1)[1]))
