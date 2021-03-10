b94637aee283e9580be4939492210b47cc00ff45
Lukas Brunner <lukas_brunner95@yahoo.de>
2021-03-10 22:20:50 +0100

    Update company_terms.R
    set up company terms

--- R/company_terms.R
+++ R/company_terms.R
@@ -1,19 +1,19 @@
-company_terms <- c("Unfiltered Tweets" = "NoFilter",
-                  "3M" = "3M",
+
+
+
+company_terms <- list(
+  "Unifiltered Tweets" = list("Unfiltered Tweets" = "NoFilter"),
+  "DAX" = list(
+
                    "Adidas" = "adidas",
                    "Allianz" = "Allianz",
-                   "American Express" = "American Express",
-                   "Amgen" = "Amgen",
-                  "Apple" = "Apple",
+
+
                   "BASF" = "BASF",
                   "Bayer" = "Bayer",
                   "Beiersdorf",
                   "BMW",
-                  "Boeing",
-                  "Caterpillar",
-                  "Chevron",
-                  "Cisco",
-                  "Coca-Cola",
+
                   "Continental",
                   "Covestro",
                   "Daimler",
@@ -23,40 +23,115 @@ company_terms <- c("Unfiltered Tweets" = "NoFilter",
                   "Deutsche Post",
                   "Deutsche Telekom",
                   "Deutsche Wohnen",
-                  "Dow",
+
                   "EON",
                   "Fresenius",
                   "Fresenius Medical Care",
-                  "Goldman Sachs",
                   "HeidelbergCement",
                   "Henkel",
-                  "Home Depot",
+                  "Infineon Teechnologies",
+                  "Linde",
+                  "Merck (German)" = "MerckDE",
+                  "MTU Aero Engines",
+                  "Münchener Rück",
+                  "RWE",
+                  "SAP",
+                  "Siements",
+                  "Volkswagen",
+                  "Vonovia"
+                  ),
+
+
+"Dow Jones Industrial" = list( "3M" = "3M",
+              "American Express" = "American Express",
+              "Amgen" = "Amgen",
+              "Apple" = "Apple",
+              "Boeing",
+              "Caterpillar",
+              "Chevron",
+              "Cisco Systems" = "Cisco",
+              "Coca-Cola",
+              "Dow",
+                "Goldman Sachs",
+                "Home Depot",
                   "Honeywell International",
                   "IBM",
-                  "Infineon Teechnologies",
                   "Intel",
                   "Johnson & Johnson" = "JohnsonJohnson",
                   "JPMorgan",
-                  "Linde",
-                  "McDonald's",
-                  "Merck (German)" = "MerckDE",
-                  "Merck (American)" = "MerckEN",
-                  "Microsoft",
-                  "MTU Aero Engines",
-                  "Münchener Rück",
-                  "NIKE" = "NIKE",
+                 "McDonald's",
+                "Merck (American)" = "MerckEN",
+              "Microsoft",
+                 "NIKE" = "NIKE",
                   "Procter & Gamble = Procter Gamble",
-                  "RWE",
+
                   "Salesforce" = "salesforce",
-                  "SAP",
-                  "Siements",
                   "Traverlers Companies",
-                  "UnitedHealth",
+            "UnitedHealth",
                   "Verizon",
                   "Visa",
-                  "Volkswagen",
-                  "Vonovia",
-                  "Walgreens Boots Alliance",
+              "Walgreens Boots Alliance",
                   "Walmart",
                   "Walt Disney"
                   )
+)
+
+
+
+
+
+
+
+us_names <- c("Dow Jones Industrial",
+              "Apple", "Amgen", "American Express", "Boeing",
+              "Caterpillar", "Salesforce", "Cisco Systems", "Chevron",
+              "Walt Disney", "Dow", "Goldman Sachs",
+              "Home Depot", "Honeywell International", "IBM",
+              "Intel", "Johnson & Johnson", "JPMorgan Chase", "Coca-Cola",
+              "McDonald's", "3M", "Merck (American)", "Microsoft",
+              "NIKE", "Procter & Gamble", "The Travelers Companies",
+              "UnitedHealth", "Visa", "Verizon",
+              "Walgreens Boots Alliance", "Walmart")
+
+us_tickers <- c("DJI", "AAPL", "AMGN", "AXP", "BA", "CAT", "CRM", "CSCO", "CVX", "DIS",
+                "DOW", "GS", "HD", "HON", "IBM", "INTC", "JNJ", "JPM", "KO",
+                "MCD", "MMM", "MRK", "MSFT", "NKE", "PG", "TRV", "UNH", "V",
+                "VZ", "WBA", "WMT")
+
+
+ger_names <- c("DAX",
+               "Covestro", "Adidas", "Allianz", "BASF", "Bayer", "Beiersdorf",
+               "BMW", "Continental", "Daimler", "Deutsche Börse",
+               "Deutsche Bank", "Delivery Hero", "Deutsche Post", "Deutsche Telekom",
+               "Deutsche Wohnen", "EON", "Fresenius Medical Care", "Fresenius",
+               "HeidelbergCement", "Henkel", "Infineon Technologies", "Linde",
+               "Merck (German)", "MTU Aero Engines",
+               "Münchener Rück", "RWE", "SAP",
+               "Siemens", "Vonovia", "Volkswagen")
+
+ger_tickers <- c("GDAXI",
+                 "1COV.DE", "ADS.DE", "ALV.DE", "BAS.DE", "BAYN.DE", "BEI.DE",
+                   "BMW.DE", "CON.DE", "DAI.DE", "DB1.DE", "DBK.DE", "DHER.DE",
+                   "DPW.DE", "DTE.DE", "DWNI.DE", "EOAN.DE", "FME.DE", "FRE.DE",
+                   "HEI.DE", "HEN3.DE", "IFX.DE", "LIN.DE", "MRK.DE", "MTX.F",
+                   "MUV2.DE", "RWE.DE", "SAP.DE", "SIE.DE", "VNA.DE", "VOW3.DE")
+
+
+company_terms_stock_us <- setNames(as.list(us_tickers), us_names)
+company_terms_stock_ger <- setNames(as.list(ger_tickers), ger_names)
+
+company_terms_stock <- list(
+  "Dow Jones Industrial" = company_terms_stock_us,
+  "DAX" = company_terms_stock_ger
+)
+
+
+# us <- readr::read_csv("C:/Users/lukas/OneDrive - UT Cloud/Data/Yahoo/USA/USA_Index_Components.csv")
+# ger <- readr::read_csv("C:/Users/lukas/OneDrive - UT Cloud/Data/Yahoo/Germany/Germany_Index_Components.csv")
+#
+# dput(us$`Company Name`)
+# dput(us$Symbol)
+#
+# dput(ger$`Company Name`)
+# dput(ger$Symbol)
+
